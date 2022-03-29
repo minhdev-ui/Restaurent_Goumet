8e57e77aabaf511f9fa9dd05810162187166e0e5
nhung190402 <2051063770@e.tlu.edu.vn>
2022-03-30 00:53:38 +0700

    Update Contact

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,4 +1,3 @@
-
 import {FaMapSigns, FaPhoneVolume} from 'react-icons/fa'
 import {AiOutlineMail} from 'react-icons/ai'
 
@@ -18,24 +17,47 @@ const Contact = () => {
                         <h1 className="uppercase tracking-5 text-40p">contact us</h1>
                     </div>
                     <div className='flex justify-around z-10'>
-                        <div className='py-12 px-7 m-5 border-y-hoverBtn border-y-2 text-center w-1/3 leading-loose flex flex-col justify-between'>
+                        <div className='py-12 px-7 m-5 border-y-hoverBtn border-y-2 text-center w-1/3 leading-loose flex flex-col justify-between bg-white'>
                             <div className='text-5xl flex justify-center text-hoverBtn'>
                                 <FaMapSigns/>
                             </div>
                             <span className='block'>Street Name, FL 54785</span>
                         </div>
-                        <div className='py-12 px-7 m-5 border-y-hoverBtn border-y-2 text-center w-1/3 leading-loose flex flex-col justify-between'>
+                        <div className='py-12 px-7 m-5 border-y-hoverBtn border-y-2 text-center w-1/3 leading-loose flex flex-col justify-between bg-white'>
                             <div className='text-5xl flex justify-center text-hoverBtn'>
                                 <FaPhoneVolume/>
                             </div>
                             <span className='block'>621-254-2147-973</span>
                         </div>
-                        <div className='py-12 px-7 m-5 border-y-hoverBtn border-y-2 text-center w-1/3 leading-loose flex flex-col justify-between'>
+                        <div className='py-12 px-7 m-5 border-y-hoverBtn border-y-2 text-center w-1/3 leading-loose flex flex-col justify-between bg-white'>
                             <div className='text-5xl flex justify-center text-hoverBtn'>
                                 <AiOutlineMail/>
                             </div>
                             <span className='block'>youremail@ipsum.com</span>
                         </div>
+                    </div>
+                    <div className='py-24 px-10 bg-gray-50 shadow-2xl -mt-16'>
+                        <form className='p-8'>
+                            <div>
+                                <label>Your name (required)</label>
+                                <input className='block p-4 mb-2 w-full border border-gray-300 focus:bg-gray-100 focus:outline-red-600' type='text' required/>
+                            </div>
+                            <div>
+                                <label>Your Email (required)</label>
+                                <input className='block p-4 mb-2 w-full border border-gray-300 focus:bg-gray-100 focus:outline-red-600' type='email' required/>
+                            </div>
+                            <div>
+                                <label>Subject</label>
+                                <input className='block p-4 mb-2 w-full border border-gray-300 focus:bg-gray-100 focus:outline-red-600' type='text'/>
+                            </div>
+                            <div>
+                                <label>Your Message</label>
+                                <textarea className='w-full block border border-gray-300 focus:bg-gray-100 focus:outline-red-600' rows='6'></textarea>
+                            </div>
+                            <div className='block'>
+                                <button className='mt-2 py-5 px-16 bg-red-600 hover:bg-hoverBtn text-white text-xl float-right'>Send</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
