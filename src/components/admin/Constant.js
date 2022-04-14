ad5890b497b0a9104f2eb13bee4e19cf6c0c953d
hatake1608 <dominh162002@gmail.com>
2022-04-14 14:29:36 +0700

    update total ui

--- src/components/admin/Constant.js
+++ src/components/admin/Constant.js
@@ -1,2 +1,7 @@
 const JSON_API = 'http://localhost:5000'
+export const menusUrl = JSON_API + '/Menus'
+export const usersUrl = JSON_API + '/users'
+export const ordersUrl = JSON_API + '/books'
+export const totalUrl = JSON_API + '/total'
+export const chefsUrl = JSON_API + '/chefs'
 export default JSON_API
--- src/components/admin/total.js
+++ src/components/admin/total.js
@@ -3,15 +3,55 @@ import {BiDollar} from 'react-icons/bi'
 import Chart from 'react-apexcharts'
 import {RiCupLine, RiFileListLine} from 'react-icons/ri'
 import { AiOutlineUsergroupDelete } from 'react-icons/ai'
-import JSON_API from './Constant'
+import { chefsUrl, menusUrl, ordersUrl, totalUrl, usersUrl } from './Constant'
 import CountUp from 'react-countup'
 export default function Total() {
-    const url = `${JSON_API}/total`
+    const url = totalUrl
+    const menuUrl = menusUrl
+    const orderUrl = ordersUrl
+    const userUrl = usersUrl
+    const chefUrl = chefsUrl
     const [total, setTotal] = useState([])
-    useEffect(() => {
-        fetch(url)
+    const [menus, setMenus] = useState([])
+    const [orders, setOrders] = useState([])
+    const [users, setUsers] = useState([])
+    const [chefs, setChefs] = useState([])
+    async function getTotal(url) {
+        const response = await fetch(url)
         .then(res => res.json())
         .then(data => setTotal(data))
+        return response
+    }
+    async function getMenus(url) {
+        const response = await fetch(url)
+        .then(res => res.json())
+        .then(data => setMenus(data))
+        return response
+    }
+    async function getOrders(url) {
+        const response = await fetch(url)
+        .then(res => res.json())
+        .then(data => setOrders(data))
+        return response
+    }
+    async function getUsers(url) {
+        const response = await fetch(url)
+        .then(res => res.json())
+        .then(data => setUsers(data))
+        return response
+    }
+    async function getChefs(url) {
+        const response = await fetch(url)
+        .then(res => res.json())
+        .then(data => setChefs(data))
+        return response
+    }
+    useEffect(() => {
+        getTotal(url)
+        getMenus(menuUrl)
+        getOrders(orderUrl)
+        getUsers(userUrl)
+        getChefs(chefUrl)
     }, [])
     console.log(total);
     
@@ -58,7 +98,9 @@ export default function Total() {
                     <RiCupLine className='text-5xl text-blue-800 leading-loose'/>
                 </span>
                 <div>
-                    <h3 className='text-4xl'>56</h3>
+                    <h3 className='text-4xl'>
+                        <CountUp end={menus.length} duration={menus.length/5}/>
+                    </h3>
                     <p className='uppercase'>total menus</p>
                 </div>
             </div>
@@ -68,9 +110,9 @@ export default function Total() {
                 </span>
                 <div>
                     <h3 className='text-4xl'>
-                        <CountUp end={56} duration={2}/>
+                        <CountUp end={parseFloat((total.map(item => item.total).reduce((acc, cur) => acc + cur, 0))/1000)} duration={3}/>K
                     </h3>
-                    <p className='uppercase'>total menus</p>
+                    <p className='uppercase'>total Revenue</p>
                 </div>
             </div>
             <div className='p-6 flex rounded-md shadow-md justify-between items-center'>
@@ -78,8 +120,10 @@ export default function Total() {
                     <RiFileListLine className='text-5xl text-blue-800 leading-loose'/>
                 </span>
                 <div>
-                    <h3 className='text-4xl'>56</h3>
-                    <p className='uppercase'>total menus</p>
+                    <h3 className='text-4xl'>
+                        <CountUp end={orders.length} duration={orders.length/5}/>
+                    </h3>
+                    <p className='uppercase'>total orders</p>
                 </div>
             </div>
             <div className='p-6 flex rounded-md shadow-md justify-between items-center'>
@@ -87,8 +131,21 @@ export default function Total() {
                     <AiOutlineUsergroupDelete className='text-5xl text-blue-700'/>
                 </span>
                 <div>
-                    <h3 className='text-4xl'>56</h3>
-                    <p className='uppercase'>total menus</p>
+                    <h3 className='text-4xl'>
+                        <CountUp end={users.length} duration={users.length/5}/>
+                    </h3>
+                    <p className='uppercase'>total users</p>
+                </div>
+            </div>
+            <div className='p-6 flex rounded-md shadow-md justify-between items-center'>
+                <span className='px-4 py-4 mr-4 rounded-full bg-blue-400 opacity-70'>
+                    <AiOutlineUsergroupDelete className='text-5xl text-blue-700'/>
+                </span>
+                <div>
+                    <h3 className='text-4xl'>
+                        <CountUp end={chefs.length} duration={chefs.length/5}/>
+                    </h3>
+                    <p className='uppercase'>total users</p>
                 </div>
             </div>
         </div>
